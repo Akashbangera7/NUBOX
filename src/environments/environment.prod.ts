092822e77ad67a0b4e19f6dcab3d97b9dc34d488
Akash Gopala Bangera <akash.a@husky.neu.edu>
2019-10-07 19:51:04 -0400

    initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA8qC8i1JQZOCfE3-whjvqUcGPJIMOdldw',
+    authDomain: 'nubox-1bb27.firebaseapp.com',
+    databaseURL: 'https://nubox-1bb27.firebaseio.com',
+    projectId: 'nubox-1bb27',
+    storageBucket: '',
+    messagingSenderId: '58450598828',
+    appId: '1:58450598828:web:64ec7bebe4aca01ac93809',
+    measurementId: 'G-BL9VX2RTFY'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyA8qC8i1JQZOCfE3-whjvqUcGPJIMOdldw',
+    authDomain: 'nubox-1bb27.firebaseapp.com',
+    databaseURL: 'https://nubox-1bb27.firebaseio.com',
+    projectId: 'nubox-1bb27',
+    storageBucket: '',
+    messagingSenderId: '58450598828',
+    appId: '1:58450598828:web:64ec7bebe4aca01ac93809',
+    measurementId: 'G-BL9VX2RTFY'
+  }
 };
 
 /*
