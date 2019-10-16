bfeb76f73341d0b2b2e13da865861a740ea1e708
Akash Gopala Bangera <akash.a@husky.neu.edu>
2019-10-15 22:23:07 -0400

    one

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyA8qC8i1JQZOCfE3-whjvqUcGPJIMOdldw',
-    authDomain: 'nubox-1bb27.firebaseapp.com',
-    databaseURL: 'https://nubox-1bb27.firebaseio.com',
-    projectId: 'nubox-1bb27',
+    apiKey: 'AIzaSyBrYKbB6vUZWzp71JhWFiomfX2aD8FoGrw',
+    authDomain: 'akash-af9fe.firebaseapp.com',
+    databaseURL: 'https://akash-af9fe.firebaseio.com',
+    projectId: 'akash-af9fe',
     storageBucket: '',
-    messagingSenderId: '58450598828',
-    appId: '1:58450598828:web:64ec7bebe4aca01ac93809',
-    measurementId: 'G-BL9VX2RTFY'
+    messagingSenderId: '105993682294',
+    appId: '1:105993682294:web:bc0d74d5d8bfe5ebf6ce57',
+    measurementId: 'G-2HT4H5FFTD'
   }
 };
