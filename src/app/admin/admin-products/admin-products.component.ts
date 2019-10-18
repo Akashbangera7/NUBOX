27638bd788ab3166c4eefd746a20d4b7504ebb2b
Akash Gopala Bangera <akash.a@husky.neu.edu>
2019-10-18 02:30:28 -0400

    search

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { ProductService } from './../../product.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
+export class AdminProductsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  products: {title: string}[];
+  filteredProducts: any[];
+  subscription: Subscription;
+  constructor(private productService: ProductService) {
+    this.subscription = this.productService.getAll().subscribe(products => this.filteredProducts = this.products = products);
+  }
 
+  filter(query: string) {
+    this.filteredProducts = (query) ? this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) : this.products;
+  }
   ngOnInit() {
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
